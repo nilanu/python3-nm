5c0fb97032cfd693eca0e65177b254420990c224
Neel Mukherjee <nilanu@gmail.com>
2020-08-02 11:27:24 -0400

    this is the first commit of the helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# This is just a simple Python 3 code to print Hello World & Current Date
+#!/usr/bin/python3
+#This code must be run using python3 & not python(2.7). If run with python 2.7 it produces erronious output
+#
+# Importing date from datetime & assigning to variable today
+from datetime import date
+today = date.today()
+# Printing the output
+print ("Hello World")
+print ("Today's Date:",today)
